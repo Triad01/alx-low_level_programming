e65ee9e4ea571369b80d7d6f7bdcdbf7c73e48dd
Triad01 <luckypee01@gmail.com>
2023-08-10 10:34:06 -0700

    quote

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - prints text to standard error
+ * Description: diplays text to standard error
+ * Return: always return 1
+*/
+int main(void)
+{
+fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+return (1);
+
+}
