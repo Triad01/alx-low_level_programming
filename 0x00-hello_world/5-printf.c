fe12e5cf27827c044da87b4234e9adc6420108b1
Triad01 <luckypee01@gmail.com>
2023-08-10 12:38:39 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -5,9 +5,13 @@
 * Description: prints text to display
 * Return: always return 0
 */
-
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,\n");
+printf("Size of char: %zu bytes\n", sizeof(char));
+printf("Size of int: %zu bytes\n", sizeof(int));
+printf("Size of float: %zu bytes\n", sizeof(float));
+printf("Size of double: %zu bytes\n", sizeof(double));
+printf("Size of long: %zu bytes\n", sizeof(long));
+printf("Size of long long: %zu bytes\n", sizeof(long long));
 return (0);
 }
