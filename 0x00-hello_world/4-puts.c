5694f180050b891ee0340de2ff6441c579682252
Triad01 <luckypee01@gmail.com>
2023-08-10 08:39:16 -0700

    pyuts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main - Entry point for the program
- * Description: Prints text to display
- * Return: Always 0 (indicating successful execution)
- */
+* main - Entry point for the program
+* Description: Prints text to display
+* Return: Always 0 (indicating successful execution)
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
