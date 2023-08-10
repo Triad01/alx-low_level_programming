1a0f7df7dee3f1bad9a068bf255d8a1b776f3102
Triad01 <luckypee01@gmail.com>
2023-08-10 13:12:35 -0700

    dadfdf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,10 @@
 */
 int main(void)
 {
-printf("Size of a char: %zu bytes\n", sizeof(char));
-printf("Size of an int: %zu bytes\n", sizeof(int));
-printf("Size of a float: %zu bytes\n", sizeof(float));
-printf("Size of a double: %zu bytes\n", sizeof(double));
-printf("Size of a long: %zu bytes\n", sizeof(long));
-printf("Size of a long long: %zu bytes\n", sizeof(long long));
+printf("Size of a char: %zu byte(s)\n", sizeof(char));
+printf("Size of an int: %zu bytes(s)\n", sizeof(int));
+printf("Size of a long int: %zu bytes(s)\n", sizeof(long));
+printf("Size of a long long int: %zu bytes(s)\n", sizeof(long long));
+printf("Size of a float: %zu bytes(s)\n", sizeof(float));
 return (0);
 }
