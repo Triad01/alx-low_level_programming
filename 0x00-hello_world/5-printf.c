a06b54f662320161c7c06d853b0fb9d95a69ffec
Triad01 <luckypee01@gmail.com>
2023-08-10 08:50:11 -0700

    5-print file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - entry into our program
+* Description: prints text to display
+* Return: always return 0
+*/
+
+int main(void)
+{
+printf("with proper grammar, but the outcomeme is a piece of art,");
+return (0);
+}
