f3e008480b8e03be20a971d2f03019aba8522ace
Triad01 <luckypee01@gmail.com>
2023-08-10 13:36:52 -0700

    adfadf

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 /**
-* main - shows the sizes of types
-* Description: displays the sizes to output
-* Return: always return 0
+* main - Entry point
+* Description: Prints the size of each types
+* Return: Always 0 (Success)
 */
 int main(void)
 {
 printf("Size of a char: %d byte(s)\n", sizeof(char));
-printf("Size of an int: %d bytes(s)\n", sizeof(int));
-printf("Size of a long int: %d bytes(s)\n", sizeof(long int));
-printf("Size of a long long int: %d bytes(s)\n", sizeof(long long int));
-printf("Size of a float: %d bytes(s)\n", sizeof(float));
+printf("Size of an int: %d byte(s)\n", sizeof(int));
+printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+printf("Size of a float: %d byte(s)\n", sizeof(float));
 return (0);
 }
