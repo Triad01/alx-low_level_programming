03f74682c557e418aae489a25df652066d81d432
Triad01 <luckypee01@gmail.com>
2023-08-10 08:10:16 -0700

    puts file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+/**
+ * main - Entry point for the program
+ * Description: Prints text to display
+ * Return: Always 0 (indicating successful execution)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle \n");
-	return (0);
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
+
