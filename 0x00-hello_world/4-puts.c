197d18ea442b4a7b8c8d39bafe0581b7730a6621
Triad01 <luckypee01@gmail.com>
2023-08-10 08:36:50 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,10 +4,8 @@
  * Description: Prints text to display
  * Return: Always 0 (indicating successful execution)
  */
-
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle\n");
 return (0);
 }
-
