c74ec497d1b3c6899192f9c2c4a2eabcd3d39fa6
Triad01 <luckypee01@gmail.com>
2023-08-11 15:25:17 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - display numbers to stdout
+* Description: prints single digits of base 10
+* Return: always return 0 (successful)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		printf("%d", i);
+	}
+		return (0);
+}
