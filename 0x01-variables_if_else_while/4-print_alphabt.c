d91715a044def867c8a632ac98420ca951ee4282
Triad01 <luckypee01@gmail.com>
2023-08-11 14:59:03 -0700

    adfdf

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+* main - prints text to stdout
+* Description: prints lowercased texts without q
+* Return: always return 0 (successuful)
+*/
+int main(void)
+{
+	char small_letters, small_letters_no_o;
+
+	small_letters_no_o = strstr(o, small_letters);
+	for (small_letters = 'a'; small_letters <= 'z'; small_letters++)
+	{
+		small_letters_no_e = strchr(e, small_letters);
+		small_letters_no_q = strchr(e, small_letters);
+		puthr(small_letters_no_e, small_letters_no_q)
+	}
+		putchar('\n');
+		return (0);
+}
