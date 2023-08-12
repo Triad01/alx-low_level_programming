148ca3d711a9f0d72940283311606fd45bdbedfb
Triad01 <luckypee01@gmail.com>
2023-08-11 18:24:41 -0700

    dadfa

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+* main - prints single digit numbers to stdout
+* Description: prints all possible combinations
+* Return: always return 0 (successful)
+*/
+
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers <= 9; numbers++)
+		putchar('0' + numbers);
+	if (numbers != 9)
+	{
+		putchar(',');
+		putchar(' ');
+	}
+	else
+	{
+		putchar('\n');
+	}
+		return (0);
+}
