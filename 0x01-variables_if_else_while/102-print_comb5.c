ae0117f25df34a39a5cec3e85148cda20c8b5aa4
Triad01 <luckypee01@gmail.com>
2023-08-12 21:30:52 -0700

    adfa

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+* main - prints numbers to stdout
+* Descrition: prints posible combo of 2 digits
+* Return: always 0 (successful)
+*/
+int main(void)
+{
+	int i, j, tens_i, ones_i, tens_j, ones_j;
+
+	for (i = 0; i < 100; i++)
+	{
+		for (j = i; j < 100; j++)
+		{
+			tens_i = i / 10;
+			ones_i = i % 10;
+			tens_j = j / 10;
+			ones_j = j % 10;
+			putchar('0' + tens_i);
+			putchar('0' + ones_i);
+			putchar(' ');
+			putchar('0' + tens_j);
+			putchar('0' + ones_j);
+			if (i < 90 || j < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	return (0);
+}
