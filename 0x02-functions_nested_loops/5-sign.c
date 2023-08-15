422f8f55ea33d39ef3548bc27ae49a6893f2d8d8
Triad01 <luckypee01@gmail.com>
2023-08-15 09:11:10 -0700

    5 rint

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign - prints sign
+* @n: first operand
+* Description: prints sign present
+* Return: always 0
+*/
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('1');
+		return (-1);
+	}
+}
