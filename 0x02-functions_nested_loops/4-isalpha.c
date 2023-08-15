4939036cd248a2136c655fa4cfbf202bd97ebd55
Triad01 <luckypee01@gmail.com>
2023-08-15 08:40:22 -0700

    4-print

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _isalpha - checks for alphabetic charactier
+* Description: checks characters
+* Return: always return 0
+*/
+int _isalpha(int c)
+{
+	if (c >= 'a' || c <= 'z' || c >= 'A' || c <= 'Z')
+	{
+		return (1);
+	}
+		return (0);
+	return (0);	
+}
