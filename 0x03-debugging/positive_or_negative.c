8e765489bfded5ae38ed546a3ebba9c90bdae0e7
Triad01 <luckypee01@gmail.com>
2023-08-16 05:38:59 -0700

    test

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,5 +1,3 @@
-#include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
 /**
 * positive_or_negative - check numbers
@@ -10,8 +8,6 @@
 
 int positive_or_negative(int n)
 {
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
