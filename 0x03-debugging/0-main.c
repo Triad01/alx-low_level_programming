c3e2c04e6c326dea7c34fd1c171abe6aa9d8fc9a
Triad01 <luckypee01@gmail.com>
2023-08-16 04:57:16 -0700

    test

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -7,6 +7,14 @@
 int main(void)
 {
 
-	positive_or_negative(-1443434);
+	positive_or_negative(num);
+	if (number == 0)
+	{
+		printf("%d is zero\n", number);
+	} else if (number < 0)
+	{
+		printf("%d is negative\n", number);
+	}
+		printf("%d is positive\n", number);
 	return (0);
 }
