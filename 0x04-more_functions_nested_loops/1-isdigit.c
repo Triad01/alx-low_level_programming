70a2c544264aa998882abcee4a3f4fdcd37fd1d9
Triad01 <luckypee01@gmail.com>
2023-08-17 05:10:50 -0700

    adfdf

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -7,11 +7,11 @@
 */
 int _isdigit(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	if (c >= '0' && c <= '9')
 	{
-		return (0);
-	}
 		return (1);
+	}
+		return (0);
 	return (1);
 }
 
