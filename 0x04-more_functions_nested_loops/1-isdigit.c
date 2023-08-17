c1fa3c95bd992343153e2f316d7184292905171d
Triad01 <luckypee01@gmail.com>
2023-08-17 04:07:03 -0700

    added files

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* _isdigit - checks for digits
+* @c: operand
+* Return: always 0 (successful)
+*/
+int _isdigit(int c)
+{
+	while ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'A'))
+	{
+		return (0);
+	}
+		return (1);
+	return (1);
+}
+
---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* mul - multiplies two values
+* @numb1: first operand
+* @numb2: second operand
+*/
+int mul(int numb1, int numb2)
+{
+	return (numb1 * numb2);
+}
