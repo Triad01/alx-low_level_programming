993d12acc0d024a93716b4ce33bda420a03b3782
Triad01 <luckypee01@gmail.com>
2023-08-17 04:55:08 -0700

    fadf

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -4,9 +4,11 @@
 * mul - multiplies two values
 * @numb1: first operand
 * @numb2: second operand
+* Return: always 0 (successful)
 */
 int mul(int numb1, int numb2)
 {
-	return (numb1 * numb2);
 	_putchar('\n');
+	return (numb1 * numb2);
+
 }
