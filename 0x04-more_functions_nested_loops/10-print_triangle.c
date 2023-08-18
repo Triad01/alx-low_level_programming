ff142cf995e2c349ed8ba4df7f8512c26f2ece76
Triad01 <luckypee01@gmail.com>
2023-08-17 23:21:05 -0700

    triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_triangle - prints triangle to stddout
+ * @size: length of triangle
+ * Return: alaways 0 (successful)
+ */
+void print_triangle(int size)
+{
+	int i, j;
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 0; j < size - 1; j++)
+			_putchar(' ');
+		for (j = 0; j < i; j++)
+			_putchar('#');
+		_putchar('\n');
+	}
+}
