7df9efd65fd28d81f476aa4b3177e097343f44f9
Triad01 <luckypee01@gmail.com>
2023-08-17 23:24:34 -0700

    triangles

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -6,13 +6,16 @@
  */
 void print_triangle(int size)
 {
-	int i, j;
-	for (i = 1; i <= size; i++)
 	{
-		for (j = 0; j < size - 1; j++)
-			_putchar(' ');
-		for (j = 0; j < i; j++)
-			_putchar('#');
-		_putchar('\n');
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 0; j < size - 1; j++)
+				_putchar(' ');
+			for (j = 0; j < i; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
 	}
 }
