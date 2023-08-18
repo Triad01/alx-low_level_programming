ada8152d2f4017a6f72aa3f159a0da987052730f
Triad01 <luckypee01@gmail.com>
2023-08-18 03:07:31 -0700

    fizz-buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,8 +12,6 @@ int main(void)
 
 		for (i = 1; i <= 100; i++)
 		{
-			if (i < 100)
-				printf(" ");
 			if (i % 3 == 0 && i % 5 == 0)
 				printf("FizzBuzz ");
 			else if (i % 3 == 0)
@@ -23,6 +21,9 @@ int main(void)
 			}
 			else
 				printf("%d ", i);
+
+			if (i < 100)
+				printf(" ");
 		}
 		printf("\n");
 		return (0);
