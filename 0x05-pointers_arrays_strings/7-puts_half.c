a43e203844be6fbd5ebeaa056227a2c45563b948
Triad01 <luckypee01@gmail.com>
2023-08-21 15:00:47 -0700

    adfdf

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -6,18 +6,22 @@
 */
 void puts_half(char *str)
 {
-	int i, start;
+	int length, start;
 
-	i = 0;
+	length = 0;
 
-	while (str[i] != '\0')
+	while (str[length] != '\0')
 	{
-		i++;
+		length++;
 	}
-	start = (i - 1) / 2 + 1;
+	if (length % 2 == 0)
+		start = length / 2;
+	else
+		start = (length + 1) / 2;
 	while (str[start != '\0'])
 	{
 		_putchar(str[start]);
 		start++;
 	}
+	_putchar('\n');
 }
