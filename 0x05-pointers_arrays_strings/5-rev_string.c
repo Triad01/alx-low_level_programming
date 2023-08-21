9ead77ab95678ce638cdb2407e4c59df0425c313
Triad01 <luckypee01@gmail.com>
2023-08-21 13:11:24 -0700

    adfdf

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * rev_string - reverses a string
+ * @s: argument
+ * Return: always 0 (successful)
+ */
+void rev_string(char *s)
+{
+	int start, end, length;
+	char temp; 
+	length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	for (start = 0, end = length - 1; start < end; start++, end++)
+	{
+		temp = s[start];
+		s[start] = s[end];
+		s[end] = temp;
+	}
+}
+
