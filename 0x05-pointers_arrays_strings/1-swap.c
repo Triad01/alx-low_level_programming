3e23c77bd581399fa8f2b2fbad4dad2c0abcec70
Triad01 <luckypee01@gmail.com>
2023-08-21 05:28:20 -0700

    swap numbers

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* swap_int - swap the position of two integers
+* @a: first integer
+* @b: second integer
+* Return: always return 0
+*/
+void swap_int(int *a, int *b)
+{
+	*a = *b;
+	*b = *a;
+}
