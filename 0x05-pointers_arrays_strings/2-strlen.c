2d985e1c1e59b2389dae237cbcc022c027ebdced
Triad01 <luckypee01@gmail.com>
2023-08-21 06:16:29 -0700

    highest number

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* int _strlen - gets the length of a string
+* @s: argument
+* Return: always 0 (successful)
+*/
+int _strlen(char *s)
+{
+	int length = 0;
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+
+}
