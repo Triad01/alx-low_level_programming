ecdc80b018f06407a99a6d127d104c89fd774fb9
Triad01 <luckypee01@gmail.com>
2023-08-24 10:06:48 -0700

    print numbers

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -6,14 +6,38 @@
 */
 void print_number(int n)
 {
+
+	int i, divisor, numDigits, temp;
+	temp = n;
+	numDigits = 0;
+
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	if (n / 10)
-		print_number(n / 10);
+	while (temp != 0)
+	{
+		temp /= 10;
+		numDigits++;
+	}
 
-	_putchar((n % 10) + '0');
+	divisor = 1;
+
+	for (i = 1; i < numDigits; i++)
+		divisor *= 10;
+
+	while (divisor != 0)
+	{
+		_putchar((n / divisor) + '0');
+		n %= divisor;
+		divisor /= 10;
+	}
 }
