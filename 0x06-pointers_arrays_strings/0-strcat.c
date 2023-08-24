e413c28729926b17e7cb367b74a8a62fea6faeaa
Triad01 <luckypee01@gmail.com>
2023-08-24 09:18:32 -0700

    new concat file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,28 @@
 #include "main.h"
 
 /**
-* _strcat - Concatenates two strings.
-* @dest: The destination string to which src will be appended.
-* @src: The source string to be appended to dest.
-*
-* Return: A pointer to the resulting string dest.
-*/
+ * _strcat - Concatenates two strings.
+ * @dest: The destination string to which src will be appended.
+ * @src: The source string to be appended to dest.
+ *
+ * Return: A pointer to the resulting string dest.
+ */
 char *_strcat(char *dest, char *src)
 {
-	char *dest_end = dest;
+	int dest_len, i;
 
-	while (*dest_end != '\0')
+	dest_len = 0;
+
+	while (dest[dest_len] != '\0')
 	{
-		dest_end++;
+		dest_len++;
 	}
-	while (*src != '\0')
+
+	for (i = 0; src[i] != '\0'; i++)
 	{
-		*dest_end = *src;
-		dest_end++;
-		src++;
+		dest[dest_len + i] = src[i];
 	}
-	*dest_end = '\0';
+	dest[dest_len + i] = '\0';
+
 	return (dest);
 }
-
