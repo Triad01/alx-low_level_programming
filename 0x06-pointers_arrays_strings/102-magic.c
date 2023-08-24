f7ef20facd568758c77edd3e8ce37c6b9211d06d
Triad01 <luckypee01@gmail.com>
2023-08-24 10:21:45 -0700

    dafdf

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
-
 /**
-* main - Prints the value of an element in an array.
-*
-* Return: Always 0.
+* main - prints 98
+* Return: always 0 (successful)
 */
 int main(void)
 {
-	int a[5] = {98, 198, 298, 398, 498};
+	int n;
+	int a[5];
 	int *p;
 
-	p = a + 2;
-	printf("a[2] = %d\n", *(p));
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write your line of code here...
+	 * Remember:
+	 * - you are not allowed to use a
+	 * - you are not allowed to modify p
+	 * - only one statement
+	 * - you are not allowed to code anything else than this line of code
+	 */
+	*(p + 5) = 98;
+	/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
-
