638a83213566b8af36e683b77175bf0bbbbb1cd0
Triad01 <luckypee01@gmail.com>
2023-08-24 09:28:02 -0700

    mozart

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -8,16 +8,17 @@
 */
 char *leet(char *str)
 {
-	char leet_map[10] = {'o', 'l', '\0', 'e', 'a', '\0', 't', 'L', '\0', 'T'};
 	int i, j;
+	char leet_letters[] = "aAeEoOtTlL";
+	char leet_numbers[] = "4433007711";
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		for (j = 0; j < 10; j++)
+		for (j = 0; leet_letters[j] != '\0'; j++)
 		{
-			if (str[i] == leet_map[j] || str[i] == leet_map[j] - 32)
+			if (str[i] == leet_letters[j])
 			{
-				str[i] = j + '0';
+				str[i] = leet_numbers[j];
 				break;
 			}
 		}
@@ -25,4 +26,3 @@ char *leet(char *str)
 
 	return (str);
 }
-
