274eb133eecc11f4b6d7e0ff457b510e1d73b6eb
Triad01 <luckypee01@gmail.com>
2023-08-28 08:44:03 -0700

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _memcpy - copy address from one memory to another
+* @dest: first argument
+* @src: second argument
+* @n: third argument
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
