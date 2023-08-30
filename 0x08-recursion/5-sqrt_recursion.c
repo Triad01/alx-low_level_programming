12de3223d1a0c984f1149d73aff1a783cf966295
Triad01 <luckypee01@gmail.com>
2023-08-30 11:56:58 -0700

    swuare root of numbers

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,14 +1,39 @@
 #include "main.h"
+
 /**
-* int _sqrt_recursion - prints the square root of a number
-* @c: argument
-* Return: always 0 (successful)
+* sqrt_helper - helper function for square root calculation
+* @n: the number for which to calculate the square root
+* @guess: current guess for the square root
+*
+* Return: the square root of n or -1 if not found
 */
+int sqrt_helper(int n, int guess)
+{
+	if (guess * guess == n)
+	{
+		return (guess);
+	}
+	else if (guess * guess > n)
+	{
+		return (-1);
+	}
+	else
+	{
+		return (sqrt_helper(n, guess + 1));
+	}
+}
+/**
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: the number for which to calculate the square root
+ *
+ * Return: the square root of n or -1 if not found
+ */
 int _sqrt_recursion(int n)
 {
-	if (n < 1)
-		return (1);
-	else 
-		n * _sqrt_recursion(x-1);
+	if (n < 0)
+	{
+		return (-1);
+	}
 
+	return (sqrt_helper(n, 0));
 }
