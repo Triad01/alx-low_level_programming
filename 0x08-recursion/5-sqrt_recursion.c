c6e54d850409cb7480447f0a658ad08d440ab430
Triad01 <luckypee01@gmail.com>
2023-08-30 11:30:34 -0700

    prime numbers

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* int _sqrt_recursion - prints the square root of a number
+* @c: argument
+* Return: always 0 (successful)
+*/
+int _sqrt_recursion(int n)
+{
+	if (n < 1)
+		return (1);
+	else 
+		n * _sqrt_recursion(x-1);
+
+}
