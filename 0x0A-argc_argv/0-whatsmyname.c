0306da2e8abf276df14df10b5b3206bbed1e9885
Triad01 <luckypee01@gmail.com>
2023-09-01 09:07:26 -0700

    print name of program

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* main - entry into our program
+* @argc: number of command line arguments
+* @argc: array of command line arguments with size argc;
+*/
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
