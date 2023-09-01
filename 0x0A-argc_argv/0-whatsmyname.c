7030d6ded74b534e6297e36fa9d0cbb802528519
Triad01 <luckypee01@gmail.com>
2023-09-01 12:01:36 -0700

    args

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,10 @@
 */
 int main(int argc, char *argv[])
 {
-	if (argc >= 0)
-	{
-	printf("%s\n", argv[0]);
-	}
-	return (0);
+	int i, totalArguments;
+	
+	totalArguments = 0;
+	for (i = 0; i < argc; i++)
+			totalArguments++;
+	printf("%d\n", totalArguments);
 }
