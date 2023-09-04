e0fdf7f4f0418334a99c2e625954f1860974491c
Triad01 <luckypee01@gmail.com>
2023-09-04 05:28:59 -0700

    malloc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,32 +8,23 @@
 */
 int main(int argc, char *argv[])
 {
-	int i, j, sum, has_letter;
+	int num1, num2, result;
 
-	sum = 0;
-
-	has_letter = 0;
-
-	if (argc == 1)
+	if (argc < 2)
 	{
-		printf("0\n");
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
 	else
-	
-		for (i = 1; i < argcc; i++)
-		{
-			for (j = 0; j < argv[i][j]; j++)
-			{
-				if (!isdigit(argv[i][j]))
-				{
-					printf("Error\n");
-					return (1)
-				}
-			}
-			sum += atoi(argv[i]);
-		}
-	printf("%d\n", sum);
+	{
+		num1 = atoi(argv[1]);
+
+		num2 = atoi(argv[2]);
+
+		result = num1 * num2;
+
+		printf("%d\n", result);
+	}
 	return (0);
 }
 
