aaaf5fa556c092d5be8b0419ef192448f6510b3b
Triad01 <luckypee01@gmail.com>
2023-09-11 09:20:39 -0700

    adfad

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -1,4 +1,5 @@
 #include "dog.h"
+#include <stddef.h>
 /**
 * init_dog - structure for dog
 * @d: pointer to struct dog data type
@@ -8,8 +9,10 @@
 */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d != NULL)
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
