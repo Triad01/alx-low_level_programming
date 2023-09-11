a7571205bbd39f88981027177cdc1583e7f45714
Triad01 <luckypee01@gmail.com>
2023-09-11 08:15:28 -0700

    first task

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+* struct dog - defined data type
+* @name: dog name
+* @age: dog age
+* @owner: owner of dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
