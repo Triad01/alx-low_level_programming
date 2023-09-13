a0cc49bf426882e1ec04c3050a0abc9b4314a315
Triad01 <luckypee01@gmail.com>
2023-09-13 05:52:48 -0700

    task 0 corrected

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,5 +1,9 @@
 #include "function_pointers.h"
-
+/**
+* print_name - prints name to stdout
+* @name: name to be printed
+* @f: function to print name
+*/
 void print_name(char *name, void (*f)(char *))
 {
 	f(name);
