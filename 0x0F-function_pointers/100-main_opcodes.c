5e98d82418ff5d0e6fa50c0b16511b96379de630
Triad01 <luckypee01@gmail.com>
2023-09-13 19:17:33 -0700

    adfd

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-void print_opcodes(int num_bytes, char *binary_path);
+void print_main_opcodes(int num_bytes);
 
 int main(int argc, char *argv[]) {
-	if (argc != 3) {
-		printf("Usage: %s number_of_bytes binary_path\n", argv[0]);
+	if (argc != 2) {
+		printf("Usage: %s number_of_bytes\n", argv[0]);
 		return 1;
 	}
 
@@ -17,31 +16,22 @@ int main(int argc, char *argv[]) {
 		return 2;
 	}
 
-	print_opcodes(num_bytes, argv[2]);
+	print_main_opcodes(num_bytes);
 
 	return 0;
 }
 
-void print_opcodes(int num_bytes, char *binary_path) {
-	char command[100];
-	sprintf(command, "objdump -d -j.text -M intel %s | grep ':%d' | cut -f2", binary_path, num_bytes);
-	FILE *fp = popen(command, "r");
-	if (fp == NULL) {
-		printf("Error: Failed to execute objdump\n");
-		exit(1);
-	}
+void print_main_opcodes(int num_bytes) {
+	int i;
+	unsigned char *main_ptr = (unsigned char *)main;
 
-	char opcode[3];
-	while (fread(opcode, 2, 1, fp) > 0) {
-		printf("%s", opcode);
-		if (num_bytes <= 2) {
+	for (i = 0; i < num_bytes; i++) {
+		printf("%02x", main_ptr[i]);
+		if (i < num_bytes - 1) {
+			printf(" ");
+		} else {
 			printf("\n");
-			break;
 		}
-		printf(" ");
-		num_bytes -= 2;
 	}
-
-	pclose(fp);
 }
 
