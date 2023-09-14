eb8647c368fdfb17c1a89fcee4e3350dfe32221d
Triad01 <luckypee01@gmail.com>
2023-09-13 19:13:50 -0700

    asdf

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,45 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
-* print_opcodes - opcodes
-* @start: starting point
-* @num_bytes: number of bytes
-*/
-void print_opcodes(unsigned char *start, size_t num_bytes)
-{
-	size_t i;
+#include <string.h>
 
-	for (i = 0; i < num_bytes; i++)
-	{
-		printf("%02x ", start[i]);
-	}
-	printf("\n");
-}
-/**
-* main - entry to program
-* @argc: argument count
-* @argv: argument vector
-* Return:always 0
-*/
-int main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
+void print_opcodes(int num_bytes, char *binary_path);
+
+int main(int argc, char *argv[]) {
+	if (argc != 3) {
+		printf("Usage: %s number_of_bytes binary_path\n", argv[0]);
+		return 1;
 	}
 
 	int num_bytes = atoi(argv[1]);
 
-	if (num_bytes <= 0)
-	{
-		printf("Error\n");
-		return (2);
+	if (num_bytes < 0) {
+		printf("Error: Number of bytes must be non-negative\n");
+		return 2;
 	}
 
-	unsigned char *main_start = (unsigned char *)main;
+	print_opcodes(num_bytes, argv[2]);
 
-	print_opcodes(main_start, num_bytes);
-
-	return (0);
+	return 0;
 }
+
+void print_opcodes(int num_bytes, char *binary_path) {
+	char command[100];
+	sprintf(command, "objdump -d -j.text -M intel %s | grep ':%d' | cut -f2", binary_path, num_bytes);
+	FILE *fp = popen(command, "r");
+	if (fp == NULL) {
+		printf("Error: Failed to execute objdump\n");
+		exit(1);
+	}
+
+	char opcode[3];
+	while (fread(opcode, 2, 1, fp) > 0) {
+		printf("%s", opcode);
+		if (num_bytes <= 2) {
+			printf("\n");
+			break;
+		}
+		printf(" ");
+		num_bytes -= 2;
+	}
+
+	pclose(fp);
+}
+
