4915b0aad813586ca22e02657f4c99031d815817
Triad01 <luckypee01@gmail.com>
2023-09-14 00:21:45 -0700

    opcodes

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,37 +1,44 @@
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-void print_opcodes(int num_bytes);
+/**
+ * main - Entry point
+ *
+ * Description: A program that prints the opcodes
+ *              of its own main function.
+ *              Usage: ./main number_of_bytes
+ *
+ * @argc: argument counter
+ * @argv: argument vector
+ *
+ * Return: Always Successful
+ */
 
-int main(int argc, char *argv[]) {
-	if (argc != 2) {
-		printf("Usage: %s number_of_bytes\n", argv[0]);
-		return 1;
+int main(int argc, char *argv[])
+{
+	int index, nbytes;
+	char *ptr = (char *) main;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
 	}
 
-	int num_bytes = atoi(argv[1]);
-
-	if (num_bytes <= 0) {
-		printf("Error: Number of bytes must be positive\n");
-		return 2;
+	nbytes = atoi(argv[1]);
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
 	}
 
-	print_opcodes(num_bytes);
-
-	return 0;
-}
-
-void print_opcodes(int num_bytes) {
-	unsigned char *main_ptr = (unsigned char *)main;
-	int i;
-
-	for (i = 0; i < num_bytes; i++) {
-		printf("%02x", main_ptr[i]);
-		if (i < num_bytes - 1) {
+	for (index = 0; index < nbytes; index++)
+	{
+		printf("%02x", ptr[index] & 0xFF);
+		if (index != nbytes - 1)
 			printf(" ");
-		} else {
-			printf("\n");
-		}
 	}
+	printf("\n");
+	return (0);
 }
-
