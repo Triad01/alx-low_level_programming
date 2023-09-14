1b903ad63cc6d8e91f9396ff82b7e59699e9b44b
Triad01 <luckypee01@gmail.com>
2023-09-13 20:12:20 -0700

    adfd

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,43 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
-/**
-* print_opcodes - prints opcodes of function
-* @num_bytes: number of bytes
-*/
-void print_opcodes(int num_bytes)
-{
-	unsigned char *ptr = (unsigned char *)print_opcodes;
-	int i;
 
-	for (i = 0; i < num_bytes; i++)
-	{
-		printf("%02x ", ptr[i]);
-	}
-	printf("\n");
-}
-/**
-* main - entry into program
-* @argc: argument count
-* @argv: argument vector
-* Return: always 0
-*/
-int main(int argc, char *argv[])
-{
-	if (argc != 2)
-	{
-		printf("Error\n");
-		return (1);
+void print_opcodes(int num_bytes);
+
+int main(int argc, char *argv[]) {
+	if (argc != 2) {
+		printf("Usage: %s number_of_bytes\n", argv[0]);
+		return 1;
 	}
 
 	int num_bytes = atoi(argv[1]);
 
-	if (num_bytes < 0)
-	{
-		printf("Error\n");
-		return (2);
+	if (num_bytes <= 0) {
+		printf("Error: Number of bytes must be positive\n");
+		return 2;
 	}
 
 	print_opcodes(num_bytes);
 
-	return (0);
+	return 0;
 }
+
+void print_opcodes(int num_bytes) {
+	unsigned char *main_ptr = (unsigned char *)main;
+	int i;
+
+	for (i = 0; i < num_bytes; i++) {
+		printf("%02x", main_ptr[i]);
+		if (i < num_bytes - 1) {
+			printf(" ");
+		} else {
+			printf("\n");
+		}
+	}
+}
+
