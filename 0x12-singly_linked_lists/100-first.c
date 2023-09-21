4d65a20671d636103ecafc2d6c6af9e5ca93fb9a
Triad01 <luckypee01@gmail.com>
2023-09-21 20:59:23 +0100

    task 5

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,18 +1,10 @@
 #include <stdio.h>
-
-void my_constructor(void) __attribute__((constructor));
-
 /**
- * my_constructor - Function that runs before main.
+ * my_constructor - execute before main
  */
+void my_constructor(void) __attribute__((constructor));
 void my_constructor(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
-
-int main(void)
-{
-	printf("(A tortoise, having pretty good sense of a hare's nature, challenges one to a race.)\n");
-	return (0);
-}
-
