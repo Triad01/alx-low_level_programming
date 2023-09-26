6b5f78beb0a7c35041f3433e4e7897ef4050248f
Triad01 <luckypee01@gmail.com>
2023-09-26 06:32:20 +0100

    corrected

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,19 +2,25 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
-* print_listint - list the elements in the node
-* @h: header pointer to the first node
-* Return: integer whose data type is size_t
-*/
+ * print_listint - list the elements in the node
+ * @h: header pointer to the first node
+ * Return: integer whose data type is size_t
+ */
 size_t print_listint(const listint_t *h)
 {
-	size_t node_count = 0;
+	const listint_t *temp;
+	int len = 0;
 
-	while (h != NULL)
+	temp = h;
+	if (temp == NULL)
+		return ((size_t)(NULL));
+
+	while (temp != NULL)
 	{
-		printf("%d\n", h->n);
-		h = h->next;
-		node_count++;
+		printf("%d\n", temp->n);
+		temp = temp->next;
+		len++;
 	}
-	return (node_count);
+
+	return (len);
 }
