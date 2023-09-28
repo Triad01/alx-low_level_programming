4740426fa9f73c4717b559f569111ec33e8b227c
Triad01 <luckypee01@gmail.com>
2023-09-28 07:59:53 -0700

    initial commit

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* binary_to_unit - converts binary to unsigned int
+* @b: pointer to string of 0 and 1 characters
+* Return: unsigned integer 
+*/
+unsigned int binary_to_unit(const char *b)
+{
+			
+}
