6a3e74290b3be6e74a40fdff0ae91b716443c4ad
Triad01 <luckypee01@gmail.com>
2023-10-02 18:50:12 +0100

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,75 +1,62 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <fcntl.h>
 #include <unistd.h>
-#include <sys/stat.h>
-#include "main.h"
+
 #define BUFFER_SIZE 1024
 
-/**
- * main - Copies the content of a file to another file
- * @argc: Number of command-line arguments
- * @argv: Array of command-line arguments
- * Return: 0 on success, exit with specific codes on failure
- */
 int main(int argc, char *argv[])
 {
-	FILE *file_from, *file_to;
+	int file_from, file_to, bytes_read, bytes_written;
 	char buffer[BUFFER_SIZE];
-	size_t read_result;
-
-	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
 	if (argc != 3)
 	{
-		fprintf(stderr, "Usage: %s file_from file_to\n", argv[0]);
-		return (97);
+		dprintf(2, "Usage: cp file_from file_to\n");
+		exit(97);
 	}
 
-	file_from = fopen(argv[1], "r");
-	if (file_from == NULL)
+	file_from = open(argv[1], O_RDONLY);
+	if (file_from == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 
-	file_to = fopen(argv[2], "w");
-	if (file_to == NULL)
+	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (file_to == -1)
 	{
-		fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-		fclose(file_from);
-		return (99);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
 	}
 
-	while ((read_result = fread(buffer, 1, BUFFER_SIZE, file_from)) > 0)
+	while ((bytes_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		size_t write_result = fwrite(buffer, 1, read_result, file_to);
-
-		if (write_result != read_result)
+		bytes_written = write(file_to, buffer, bytes_read);
+		if (bytes_written == -1)
 		{
-			fprintf(stderr, "Error: Can't write to %s\n", argv[2]);
-			fclose(file_from);
-			fclose(file_to);
-			return (99);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
 		}
 	}
 
-	if (ferror(file_from))
+	if (bytes_read == -1)
 	{
-		fprintf(stderr, "Error: Can't read from file %s\n", argv[1]);
-		fclose(file_from);
-		fclose(file_to);
-		return (98);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
 	}
 
-	fclose(file_from);
-	fclose(file_to);
-
-	if (chmod(argv[2], permissions) == -1)
+	if (close(file_from) == -1)
 	{
-		fprintf(stderr, "Error: Can't set permissions on %s\n", argv[2]);
-		return (100);
+		dprintf(2, "Error: Can't close fd %d\n", file_from);
+		exit(100);
+	}
+
+	if (close(file_to) == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", file_to);
+		exit(100);
 	}
 
 	return (0);
 }
-
