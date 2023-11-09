67be6749dfdc9d0fd7083385c9ec6963f75855dd
Triad01 <luckypee01@gmail.com>
2023-11-09 19:24:04 +0100

    task 6

---
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+/**
+* sum_dlistint - calculates the sum of data in a doubly linked list
+* @head: pointer to the first node on the doubly linked list
+* Return: always an integer
+*/
+int sum_dlistint(dlistint_t *head)
+{
+	int sum;
+
+	dlistint_t *traverse_ptr;
+
+	sum = 0;
+
+	if (head == NULL)
+	{
+		return (0);
+	}
+
+	traverse_ptr = head;
+
+	while (traverse_ptr->next != NULL)
+	{
+		traverse_ptr = traverse_ptr->next;
+		sum = sum + traverse_ptr->n;
+	}
+	return (sum);
+}
