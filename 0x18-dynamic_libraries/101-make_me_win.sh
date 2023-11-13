c94b9cf19a90d74014fa15604e6e1f938368d97f
Triad01 <luckypee01@gmail.com>
2023-11-13 13:11:38 +0100

    hack for jackpot

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-echo -e "#include <stdlib.h>\nint rand(void) { return 9; }" > /tmp/libhack.c
-gcc -shared -fPIC -o /tmp/libhack.so /tmp/libhack.c
-export LD_PRELOAD=/tmp/libhack.so
+wget -P .. https://raw.githubusercontent.com/Triad01/alx-low_level_programming/master/0x18-dynamic_libraries/libhack.so
+export LD_PRELOAD="$PWD/../libhack.so"
